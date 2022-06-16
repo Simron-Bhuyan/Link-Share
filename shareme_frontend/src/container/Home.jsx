a936e6057065fc90ba7be240f526baa763913063
Simron <simron0707@gmail.com>
2022-06-16 19:07:37 +0530

    saving the pin functionality added

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -8,12 +8,13 @@ import { client } from '../client';
 import logo from '../assets/logo.png';
 import Pins from './Pins';
 import { userQuery } from '../utils/data';
+import {fetchUser} from '../utils/fetchUser';
 
 const Home = () => {
   const [ToggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const userInfo = fetchUser();
 
   const scrollRef = useRef(null);
 
